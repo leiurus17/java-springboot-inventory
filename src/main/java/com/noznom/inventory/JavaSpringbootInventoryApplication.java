874b817033b8335d73aa8230dd12ebc8f31b155c
Marlon Monzon <martianworm17@gmail.com>
2018-12-24 12:13:47 +0800

    Initial commit

---
+++ src/main/java/com/noznom/inventory/JavaSpringbootInventoryApplication.java
@@ -0,0 +1,14 @@
+package com.noznom.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootInventoryApplication.class, args);
+	}
+
+}
+
